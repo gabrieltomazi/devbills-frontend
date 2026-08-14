38f4806ed3be91efe2d00d91e91d9500020e2d71
Gabriel Tomazi <tomazigabriel3@gmail.com>
2026-08-14 00:32:37 -0300

    feat(header): add user authentication and logout logic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,52 @@
 import { Activity, LogOut } from "lucide-react";
-import { Link } from "react-router";
+import { useState } from "react";
+import { Link, useLocation } from "react-router";
+import { useAuth } from "../context/authContext";
 
+interface NavLink {
+  name: string;
+  path: string;
 
+}
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const { authState, signOut } = useAuth();
+  const { pathname } = useLocation();
+
+  const isAuthenticated = !!authState.user;
+
+  const navLink: NavLink[] = [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Transações", path: "/transacoes" }
+  ]
+
+  const handleSignOut = (): void => {
+    signOut();
+  }
+
+  const renderAvatar = () => {
+    if (!authState.user) return null
+
+    if (authState.user.photoUrl) {
+      <img
+        src={authState.user.photoUrl}
+        alt={`Foto de perfil de ${authState.user.displayName}`}
+        className="w-8 h-8 rounded-full border border-gray-700"
+      />
+    }
+
+    return (
+      <div className="w-8 h-8 rounded-full bg-primary-500 flex items-center justify-center text-white">
+        {authState.user.displayName.charAt(0)}
+      </div>
+    )
+
+  }
 
   return (
     <>
-      <header className="flex justify-between container-app py-4 w-full bg-gray-900 overflow-hidden">
+      <header className="bg-gray-900 flex justify-between container-app py-4 w-full border-gray-700 overflow-hidden">
 
         {/* Logo */}
         <div className="flex items-center gap-2">
@@ -22,13 +61,19 @@ export default function Header() {
         </nav>
 
 
+
+
         <div className="flex items-center">
           <div className="h-8 w-8 rounded-full overflow-hidden mr-2">
-            <img src="https://github.com/gabrieltomazi.png" alt="users-photo" />
+            <img src={authState.user.photoUrl} alt="users-photo" />
           </div>
-          <p className="mr-4">Gabriel Tomazi</p>
-          <button type="button" className="cursor-pointer">
-            <LogOut className="text-red-500" />
+          <p className="mr-4">{authState.user.displayName}</p>
+          <button
+            type="button"
+            onClick={handleSignOut}
+            className="p-2 rounded-full "
+          >
+            <LogOut className="text-red-500 cursor-pointer" />
           </button >
         </div>
 
